4306fdd28aa2ed9a32eb67535c6e8399ea7add7d
Cameron Gutman <aicommander@gmail.com>
2014-10-25 10:05:36 -0400

    Add JNI class for ffmpeg_muxer

---
+++ app/src/main/java/org/andcast/casting/FfmpegMuxer.java
@@ -0,0 +1,21 @@
+package org.andcast.casting;
+
+public class FfmpegMuxer {
+
+    static {
+        // FFMPEG dependencies
+        System.loadLibrary("avutil-52");
+        System.loadLibrary("swresample-0");
+        System.loadLibrary("swscale-2");
+        System.loadLibrary("avcodec-55");
+        System.loadLibrary("avformat-55");
+
+        System.loadLibrary("ffmpeg_muxer");
+    }
+
+    public static native int initializeMuxer(String formatName, String fileName, int width, int height, int frameRate, int iFrameInterval);
+
+    public static native void cleanupMuxer();
+
+    public static native int submitVideoFrame(byte[] buffer);
+}
