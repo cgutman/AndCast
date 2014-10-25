8b8fa498231cfe94556365eb344bedb6ca8b47e5
Cameron Gutman <aicommander@gmail.com>
2014-10-25 09:13:22 -0400

    Add ffmpeg_muxer and ffmpeg libs

---
+++ app/src/main/jni/FfmpegMuxer/muxer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// This is a subset of the fields in the CastConfiguration object
+typedef struct _CAST_CONFIGURATION {
+    int width, height;
+    int frameRate;
+    int iFrameInterval;
+} CAST_CONFIGURATION, *PCAST_CONFIGURATION;
+
+int initializeMuxer(const char *formatName, const char *fileName, PCAST_CONFIGURATION config);
+
+int submitVideoFrame(char *data, int length);
+
+void cleanupMuxer(void);
