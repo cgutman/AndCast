72d86e33e324574696c948d283af30d4e9603712
Cameron Gutman <aicommander@gmail.com>
2014-10-25 19:55:44 -0400

    Cleanup the output thread on stop. Be a foreground service when streaming

--- app/src/main/java/org/andcast/casting/CastingService.java
+++ app/src/main/java/org/andcast/casting/CastingService.java
@@ -2,6 +2,8 @@ package org.andcast.casting;
 
 import java.io.IOException;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.hardware.display.DisplayManager;
@@ -10,9 +12,14 @@ import android.media.projection.MediaProjection;
 import android.os.Binder;
 import android.os.IBinder;
 
+import org.andcast.MainActivity;
+import org.andcast.R;
+
 public class CastingService extends Service {
 
 	private Binder binder = new CastingBinder();
+
+    private static final int NOTIFICATION_ID = 30;
 	
 	public class CastingBinder extends Binder {
 		private MediaCodecEncoder encoder;
@@ -25,6 +32,27 @@ public class CastingService extends Service {
 			this.config = config;
 		}
 
+        public void enterForeground() {
+            Intent intent = new Intent(CastingService.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent pendIntent = PendingIntent.getActivity(CastingService.this, 0, intent, 0);
+
+            Notification.Builder builder = new Notification.Builder(CastingService.this);
+            builder .setTicker("AndCast Running")
+                    .setContentTitle("AndCast Running")
+                    .setAutoCancel(false)
+                    .setOngoing(true)
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setContentIntent(pendIntent)
+                    .setContentText("AndCast is currently casting your screen");
+
+            startForeground(NOTIFICATION_ID, builder.build());
+        }
+
+        public void exitForeground() {
+            stopForeground(true);
+        }
+
 		public void start() throws IOException {
 			// Create the H264 encoder and input surface for the virtual display
 			encoder = MediaCodecEncoder.createEncoder(config);
@@ -32,6 +60,9 @@ public class CastingService extends Service {
             int res = FfmpegMuxer.initializeMuxer(config.streamMuxType, config.streamUrl,
                     config.width, config.height, config.frameRate, config.iFrameIntervalSecs);
             if (res != 0) {
+                encoder.release();
+                encoder = null;
+
                 throw new IOException("Failed to start casting");
             }
 
@@ -55,16 +86,30 @@ public class CastingService extends Service {
 				@Override
 				public void onStopped() {
 					System.out.println("onStop");
+                    FfmpegMuxer.cleanupMuxer();
+
 					encoder.release();
+                    encoder = null;
+
+                    exitForeground();
 				}
 			}, null);
+            if (display == null) {
+                FfmpegMuxer.cleanupMuxer();
+
+                encoder.release();
+                encoder = null;
+
+                throw new IOException("Failed to create virtual display");
+            }
+
+            // Become a foreground service so we're unlikely to be killed
+            enterForeground();
 		}
 
 		public void stop() {
-			// This will stop the encoder too
+			// This will trigger the rest of the cleanup process
 			display.release();
-
-            FfmpegMuxer.cleanupMuxer();
 		}
 	}
 	
